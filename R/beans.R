d5180fca146928b3312e1bff5b89fd33a632116f
Max Kuhn <mxkuhn@gmail.com>
2021-07-08 15:52:47 -0400

    initial version

---
+++ R/beans.R
@@ -0,0 +1,26 @@
+#' Dry beans
+#'
+#' @details
+#'  Koklu and Ozkan (2020) describe a data set where images of beans were
+#'  segmented and quantified. There are 13,611 beans quantified using 16
+#'  morphologic image features. The variety of each bean was labeled as one of
+#'  six values (in the column `class`).
+#'
+#' @name beans
+#' @aliases beans
+#' @docType data
+#' @return \item{beans}{a tibble}
+#'
+#' @source
+#' Koklu, M, and IA Ozkan. 2020. Multiclass Classification of Dry Beans Using
+#' Computer Vision and Machine Learning Techniques.Computers and Electronics in
+#' Agriculture, 174: 105507.
+#' \doi{10.1016/j.compag.2020.105507},
+#' \url{https://www.muratkoklu.com/datasets/}
+#'
+#'
+#' @keywords datasets
+#' @examples
+#' data(beans)
+#' str(beans)
+NULL
